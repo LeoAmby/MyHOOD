21d19b3569a1c33a5537a0d39acd9f7dfa0e6bea
Leo <leoamby@gmail.com>
2019-10-31 15:38:51 +0300

    include a forms.py with various form classes

---
+++ register/forms.py
@@ -0,0 +1,36 @@
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm
+from django import forms
+from django.contrib.auth.models import User
+from hood.models import Profile, Project
+
+
+
+
+class RegisterForm(UserCreationForm):
+    email = forms.EmailField()
+
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
+
+
+class UserUpdateForm(forms.ModelForm):
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ['username', 'email']
+
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['image']
+    
+
+class ProjectForm(forms.ModelForm):
+    model = Project
+    fields = ['title', 'photo', 'description', 'link']
+
