2327352752b4a572343d45f8e61f20c002c7c3c1
Leo <leoamby@gmail.com>
2019-10-31 15:06:27 +0300

    added the register app in the settings.py

--- neighborhood/settings.py
+++ neighborhood/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'bootstrap4'
     'hood.apps.HoodConfig',
+    'register.apps.RegisterConfig',
     'crispy_forms',
     'rest_framework',
     'django.contrib.admin',
