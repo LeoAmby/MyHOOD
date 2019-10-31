fef74327e68e5918de8cbbb7dc3765b046762c97
Leo <leoamby@gmail.com>
2019-10-31 21:25:30 +0300

    finalize with the index view

--- neighborhood/urls.py
+++ neighborhood/urls.py
@@ -13,7 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
+from register import views as auth_views
 from django.urls import path, include
 from register import views as auth_views
 
@@ -21,6 +24,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('hood.urls')),
     path('register/', auth_views.register, name="register"),
+    path('account', include("django.contrib.auth.urls")),
 
 ]
 
