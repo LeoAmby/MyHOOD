1c650b941441a9847be2623b5d622a15fcda953a
Leo <leoamby@gmail.com>
2019-10-31 16:03:33 +0300

    created the models for user, neighborhood and business

--- hood/models.py
+++ hood/models.py
@@ -1,3 +1,22 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+from django.urls import reverse
 # Create your models here.
+
+
+
+class Neighborhood(models.Model):
+    name = models.CharField()
+    location = models.CharField()
+    occupants = models.IntegerField()
+
+class User(models.Model):
+    name = models.CharField()
+    identity = models.IntegerField()
+    neighborhood = models.ForeignKey(Neighborhood, on_delete=models.CASCADE)
+    email = models.EmailField()
+
+class Business(models.Model):
+    name = models.CharField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    neighborhood = models.ForeignKey(Neighborhood, on_delete=models.CASCADE)
--- register/forms.py
+++ register/forms.py
@@ -2,8 +2,7 @@ from django.contrib.auth import login, authenticate
 from django.contrib.auth.forms import UserCreationForm
 from django import forms
 from django.contrib.auth.models import User
-from hood.models import Profile, Project
-
+from hood.models import 
 
 
 
