c13a03a2a39918f6954d0d825f2ad473cf5ef999
Leo <leoamby@gmail.com>
2019-10-31 15:18:10 +0300

    registered the app's urls

--- neighborhood/settings.py
+++ neighborhood/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'bootstrap4'
+    'bootstrap4',
     'hood.apps.HoodConfig',
     'register.apps.RegisterConfig',
     'crispy_forms',
